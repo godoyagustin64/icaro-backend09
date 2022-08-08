2d6ca1edb61797a156bf0639fba4a44a6b9c1b7f
Agustin Godoy <godoyagustinnahuel93@gmail.com>
2022-08-08 20:45:04 -0300

    add routes

---
+++ src/routes/product.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/products", (req,res) => {
+
+})
+
+router.get("/products/:id", (req, res) => {
+
+})
+
+router.post("/products", (req,res) => {
+
+})
+
+router.put("/products/:id", (req,res) => {
+
+})
+
+router.patch("/products/:id", (req,res) => {
+
+})
+
+router.delete("/products/:id", (req,res) => {
+
+})
+
+
+module.exports = router;
